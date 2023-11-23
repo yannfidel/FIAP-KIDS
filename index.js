798a33bf2007b4186147f77305e57579ef1cec6e
TomasMarquesCostacurta <131001123+TomasMarquesCostacurta@users.noreply.github.com>
2023-11-23 11:27:49 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,12 +2,106 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const read = require("body-parser/lib/read");
 
 
 const dbName = "partytime";
 const port = 3000;
 const app = express();
 
+const UserSchema = new mongoose.Schema({
+    name : { type : String },
+    email : {type : String, required : true },
+    password : {type : String, required: true}
+    });
+    
+    const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
+
+
+const produtoSchema = new mongoose.Schema({
+        codigo : {
+            type : Number,
+            required : true
+        },
+        descricao : {
+            type : String
+        },
+        fornecedor : {
+            type : String
+        },
+        DataFabricacao : {
+            type : Date,
+            required : true
+        },
+        QuantidadeEstoque : {
+            type : Number,
+            required : true
+        }
+    })
+    
+    const Produto = mongoose.model("Produto", produtoSchema);
+    
+    modules.exports = Produto;
+
+    app.post("/fiapkids"), async (req, res) => {
+        const codigo = req.body.codigo;
+        const descricao = req.body.descricao;
+        const fornecedor = req.body.fornecedor;
+        const DataFabricacao = req.body.DataFabricacao;
+        const QuantidadeEstoque = req.body.QuantidadeEstoque
+
+        if(codigo == null || DataFabricacao == null || QuantidadeEstoque == null){
+            return res.status(400).json({error : "Preencher todos os campos!!"});
+        }
+        const codigoExiste = await Produto.findOne({codigo : codigo});
+        if(codigoExiste){
+            return res.status(400).json({error : "O codigo informado já existe"});
+        }
+        const produto = new produtoSchema({
+            codigo : codigo,
+            descricao : descricao,
+            fornecedor : fornecedor,
+            DataFabricacao : DataFabricacao,
+            QuantidadeEstoque : QuantidadeEstoque
+        })
+        try {
+            const newproduto = await produto.save();
+            res.json({error : null, msg: "Cadastro do produto ok", ProdutoId : newproduto._id});
+        } catch(error) {};
+    }
+    app.get("/cadastroproduto", async (req, res) => {
+        res.sendFile(__dirname + "/index.html")
+    })
+    
+    app.post("/cadastrousuario", async (req, res) => {
+        const nome = req.body.nome;
+        const email = req.body.email;
+       const password = req.body.password;
+        //validação de campos
+        if(nome == null || email == null || password == null){
+        return res.status(400).json({error : "Preencher todos os campos!!!"});
+        }
+        //teste de duplicidade
+        const emailExiste = await Usuario.findOne({email : email});
+        if(emailExiste){
+        return res.status(400).json({error : "O email informado já existe"});
+        }})
+    
+        const usuario = new UserSchema({
+            nome: nome,
+            email: email,
+            password : password
+            });
+        try {
+        const newUsuario = await usuario.save();
+        res.json({ error: null, msg: "Cadastro ok", UsuarioId: newUsuario._id });
+        } catch (error) {};
+            //rota de get de formulario
+            app.get("/cadastrousuario", async (req, res) => {
+            res.sendFile(__dirname + "/index.html");
+    });
 
 app.get("/", (req, res)=> {
     //primeira rota de teste
@@ -17,3 +111,5 @@ app.get("/", (req, res)=> {
     app.listen(port, ()=>{
     console.log(`O backend está rodando na porta ${port}`)
     })
+
+
